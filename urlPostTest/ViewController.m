bb89f531eda942a53c75360bba17e5e697d6d6a2
Christoph Wolff <github@pixelpoesie.com>
2013-05-15 22:51:07 +0200

    First Things first

--- urlPostTest/ViewController.m
+++ urlPostTest/ViewController.m
@@ -18,6 +18,33 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *post = [NSString stringWithFormat:@"shift=test"];
+    
+    NSData *postData = [post dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:NO];
+    NSString *postLength = [NSString stringWithFormat:@"%d", [post length]];
+    
+    NSURL *url = [NSURL URLWithString:@"http://pixelpoesie.com/urlposttest/post.php"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
+    [request setHTTPMethod:@"POST"];
+    NSLog(@"%@", post);
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    [request setHTTPBody:postData];
+    
+    //[NSURLConnection connectionWithRequest:request delegate:nil];
+    //Funktion unten 
+      
+    
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0ul);
+    dispatch_async(queue, ^{
+        NSURLResponse *response = nil;
+        NSError *error = nil;
+        
+       [NSURLConnection sendSynchronousRequest:request
+                                                     returningResponse:&response
+                                                                 error:&error];    
+    });
+    
 }
 
 - (void)didReceiveMemoryWarning
